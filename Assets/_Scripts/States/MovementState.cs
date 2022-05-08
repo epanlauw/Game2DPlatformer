b5d28d908b7c50f5fa2d56132fbfdf77f4c31839
epanlauw <epanlauw6@gmail.com>
2022-05-08 16:24:37 +0700

    Implement ScriptableObject to Agent Data

--- Assets/_Scripts/States/MovementState.cs
+++ Assets/_Scripts/States/MovementState.cs
@@ -8,9 +8,6 @@ public class MovementState : State
     [SerializeField]
     protected MovementData movementData;
     public State IdleState;
-
-    public float acceleration, deacceleration, maxSpeed;
-
     private void Awake()
     {
         movementData = GetComponentInParent<MovementData>();
@@ -71,12 +68,12 @@ public class MovementState : State
     {
         if (Mathf.Abs(movementVector.x) > 0)
         {
-            movementData.currentSpeed += acceleration * Time.deltaTime;
+            movementData.currentSpeed += agent.agentData.acceleration * Time.deltaTime;
         }
         else
         {
-            movementData.currentSpeed -= acceleration * Time.deltaTime;
+            movementData.currentSpeed -= agent.agentData.deacceleration * Time.deltaTime;
         }
-        movementData.currentSpeed = Mathf.Clamp(movementData.currentSpeed, 0, maxSpeed);
+        movementData.currentSpeed = Mathf.Clamp(movementData.currentSpeed, 0, agent.agentData.maxSpeed);
     }
 }
